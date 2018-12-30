1b7a507b036414674c149fbe3efe7b8b4da620c1
novanadhagara <32868804+novanadhagara@users.noreply.github.com>
2018-12-30 23:22:26 +0700

    Add files via upload

---
+++ kontrol.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class kontrol : MonoBehaviour {
+    Rigidbody2D fisika;
+    public Text teksSkor;
+    public float gayaKeatas;
+    public int skor;
+    public GameObject panelGameOver;
+
+    // Use this for initialization
+    void Start() {
+        fisika = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+        {
+            fisika.velocity = Vector2.zero;
+            fisika.AddForce(new Vector2(0f, gayaKeatas));
+        }
+       
+    }
+    public void game_ulangin() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+     }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("skore"))
+        {
+            skor += 10;
+            teksSkor.text = "Score " + skor.ToString();
+        }else if(collision.gameObject.CompareTag("rintangan")) {
+            panelGameOver.SetActive(true);
+        }
+    }
+}
---
+++ rintangan.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rintangan : MonoBehaviour {
+    Rigidbody2D fisika;
+    public float kecepatan;
+    public float posisiAwal;
+    public float posisiRandom;
+    public float posisiTabrakan;
+    ///0.2, -5
+	// Use this for initialization
+	void Start () {
+        posisiAwal = transform.position.x;
+
+        posisiTabrakan = transform.position.x;
+
+        fisika = GetComponent<Rigidbody2D>();
+        fisika.velocity = new Vector2(-1f * kecepatan, 0f);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void resetObject()
+    {
+        Random.InitState(System.DateTime.Now.Millisecond);
+        posisiRandom = Random.Range(0.2f, -5f)*0.8f;
+        
+
+        fisika.velocity = Vector2.zero;
+        transform.position = new Vector3(posisiAwal,posisiRandom,transform.position.z);
+        fisika.velocity = new Vector2(-1f * kecepatan, 0f);
+
+
+
+    }
+    void OnTriggerEnter2D(Collider2D collision)
+     {
+     if(collision.gameObject.CompareTag("ujung"))
+     {
+            resetObject();
+     }
+
+    }
+}
+
