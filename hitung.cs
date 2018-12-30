f3b90816b260d89bad82acfb57b5c27a54026a87
novanadhagara <32868804+novanadhagara@users.noreply.github.com>
2018-12-30 23:21:52 +0700

    Add files via upload

---
+++ hitung.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hitung : MonoBehaviour {
+    coba tcoba;
+	// Use this for initialization
+	void Start () {
+        tcoba = GetComponent<coba>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        kalkulasi(tcoba.score);
+	}
+    private void kalkulasi (int nilai) {
+        print("hasil "+nilai.ToString()+"nilai"+(nilai*2).ToString());
+
+    }
+}
